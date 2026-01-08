e97cdcc1d02f8f39f967070090b99556ab59db9b
Zemin Chen <158266635+ZeminnChen@users.noreply.github.com>
2026-01-08 23:00:00 +0100

    Add meditation audios

--- Assets/Scripts/LanternAction.cs
+++ Assets/Scripts/LanternAction.cs
@@ -1,21 +1,72 @@
 using UnityEngine;
+using System.Collections;
 
 public class LanternAction : MonoBehaviour
 {
+    [Header("Light config")]
     public Light lantern; 
-    // public AudioSource meditationAudio; // Arrastra aquí tu clip de paz
+    public float duration = 2.0f;
+    public float maxIntensity = 1.0f;
+    private bool activated = false;
 
+    [Header("Menu config")]
+    public GameObject menuMeditation; 
+    private CanvasGroup menuCanvasGroup;
+
+
+    void Start()
+    {        
+        if (menuMeditation != null) {
+            menuCanvasGroup = menuMeditation.GetComponent<CanvasGroup>();
+            if (menuCanvasGroup == null) {
+                menuCanvasGroup = menuMeditation.AddComponent<CanvasGroup>();
+            }
+
+            menuMeditation.SetActive(false);
+            menuCanvasGroup.alpha = 0f;
+        }
+        
+        if (lantern != null) lantern.enabled = false;
+    }
+
+    void Update()
+    {
+        if (menuMeditation != null && menuMeditation.activeSelf)
+        {
+            Vector3 targetPos = new Vector3(Camera.main.transform.position.x, menuMeditation.transform.position.y, Camera.main.transform.position.z);
+            menuMeditation.transform.LookAt(targetPos);
+            menuMeditation.transform.Rotate(0, 180, 0);
+        }
+    }
 
     public void OnGazeClick()
     {
-        if (lantern != null){
-            lantern.enabled = true;
-            Debug.Log("Linterna iluminada por Gaze!");
+        if (!activated)
+        {
+            StartCoroutine(SwitchOnAndShowMenu());
+            activated = true;
+        }
+    }
+
+    IEnumerator SwitchOnAndShowMenu()
+    {
+        float time = 0f;
+        if (lantern != null) lantern.enabled = true;
+        if (menuMeditation != null) menuMeditation.SetActive(true);
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float progress = time / duration;
+
+            if (lantern != null) lantern.intensity = Mathf.Lerp(0f, maxIntensity, progress);
+            if (menuCanvasGroup != null) menuCanvasGroup.alpha = Mathf.Lerp(0f, 1f, progress);
+
+            yield return null;
         }
 
-
-        //if (meditationAudio != null && !meditationAudio.isPlaying){
-            //meditationAudio.Play();
-        //}
+        if (lantern != null) lantern.intensity = maxIntensity;
+        if (menuCanvasGroup != null) menuCanvasGroup.alpha = 1f;
     }
+
 }
--- Assets/Scripts/MeditationHandler.cs
+++ Assets/Scripts/MeditationHandler.cs
@@ -3,20 +3,31 @@ using UnityEngine.UI;
 
 public class MeditationHandler : MonoBehaviour
 {
-    public AudioSource backgroundMusic;
+    [Header("Audio config")]
+    public AudioSource sourceAudio;
+    public AudioClip loto;
+    public AudioClip zen;
+    public AudioClip bamboo;
 
-    public void StartMeditationClicked()
+    public void ReproduceLoto() { ReproduceAudio(loto); }
+    public void ReproduceBamboo() { ReproduceAudio(bamboo); }
+    public void ReproduceZen (){ ReproduceAudio(zen); }
+
+    private void ReproduceAudio(AudioClip clip)
     {
-        if (backgroundMusic != null)
+        if (sourceAudio != null && clip != null)
         {
-            backgroundMusic.Play();
-        }
-        else
-        {
-            Debug.LogError("Error.");
-        }
-        
+            if (sourceAudio.clip == clip && sourceAudio.isPlaying) return;
 
-        Debug.Log("Starting meditation.");
+            sourceAudio.Stop(); 
+            sourceAudio.clip = clip;
+            sourceAudio.loop = true;
+            sourceAudio.Play();
+        }
+    }
+
+    public void StopAudio()
+    {
+        if (sourceAudio != null) sourceAudio.Stop();
     }
 }
